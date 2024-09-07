435f4c16f98762209e6c1c05c95fe120ec9de917
Aryan soni <aryansoni1973@gmail.com>
2024-09-07 14:30:33 +0530

    Add Vercel Analytics, update metadata, and bump version to 0.2.0
    - Integrate Vercel Analytics for improved site performance tracking
    - Add @vercel/analytics dependency (version 1.3.1)
    - Update metadata and improve SEO for homepage and contact page
    - Add OpenGraph metadata for better social media sharing
    - Include Analytics component in RootLayout
    - Bump project version from 0.1.0 to 0.2.0
    - Update page titles and descriptions with more detailed content
    - Include keywords relevant to the site's content and purpose
    - Set metadataBase URL for consistent link generation
    - Add icon metadata for better brand representation
    
    These changes enhance the site's analytics capabilities, improve its
    visibility in search engines, and provide a more accurate representation
    of the site's content and purpose. The version bump reflects the
    significant improvements made to the project's functionality and SEO.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,25 @@
 import ContactForm from "./components/ContactForm";
 import ContactInfo from "./components/ContactInfo";
 
+export const metadata = {
+    title: "Contact Us - Aryan Soni | Frontend Developer & UI/UX Developer | Web Development Solutions",
+    description: "Experienced Frontend & UI/UX Developer in Jabalpur. Specializing in responsive, dynamic web applications with expertise in HTML, CSS, JavaScript, React, Next.js and Tailwind CSS. Let's innovate together!",
+    keywords: "contact Sonianime, anime streaming support, feedback, anime requests, customer support, get in touch",
+    metadataBase: new URL("https://soni-anime.vercel.app"),
+    openGraph: {
+        title: "Contact Us - Sonianime",
+        description: "Reach out to the Soni-Anime team with any questions or feedback. We are here to improve your anime streaming experience.",
+        url: "https://soni-anime.vercel.app/contact",
+        images: [
+            {
+                url: "/soni-anime-contact-page-cover.jpg",
+                alt: "Contact soni-anime: Aryan Soni",
+            },
+        ],
+    }
+};
+
+
 export default function Page() {
     return (
         <section className="flex flex-col justify-center px-2 mt-[15vh] md:px-10 lg:flex-row lg:gap-10 lg:items-start lg:py-responsive-padding-y">
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
@@ -6,12 +7,26 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "sonianime",
-  description: "Watch free animes",
+  title: "soni-anime - Watch and Download Free Anime | Responsive Anime Streaming",
+  description: "Sonianime is a feature-rich anime streaming site offering free anime streaming and downloading. Built with Next.js 14, Tailwind CSS, and Swiper.js, Sonianime provides a seamless, responsive design and enhanced user experience across all devices.",
+  keywords: "anime streaming, free anime, download anime, Sonianime, watch anime, responsive design, Next.js, Tailwind CSS, Swiper.js",
   icons: {
-    icon: './icons/logo.svg',
-  } 
+    icon: '/icons/logo.svg',
+  },
+  metadataBase: new URL("https://soni-anime.vercel.app"),
+  openGraph: {
+    title: "soni-anime - Watch Free Anime",
+    description: "Stream and download your favorite anime for free on Sonianime. Experience smooth navigation, fast loading, and responsive design across all devices.",
+    url: "https://soni-anime.vercel.app/",
+    images: [
+      {
+        url: "/soni-anime-cover.png",
+        alt: "Sonianime cover image",
+      },
+    ],
+  }
 };
+
 
 export default function RootLayout({ children }) {
   return (
@@ -19,8 +34,9 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} bg-black text-white flex flex-col`}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
